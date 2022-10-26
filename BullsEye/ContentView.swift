9f30d38fe31cff23a09c6f44e443e7ecab1174eb
manukant tyagi <mankant.tyagi@ranium.in>
2022-10-26 12:35:04 +0530

    Swift UI Views

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -10,12 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            VStack {
+                VStack {
+                    Text("🎯🎯🎯PUT THE BULLSEYE AS CLOSE AS YOU CAN DO")
+                    Text("89")
+                }
+                HStack {
+                    Text("1")
+                    Slider(value: .constant(50), in: 1.0...100)
+                    Text("100")
+                }
+                Button("Hit Me") {
+                }
+            }
         }
-        .padding()
+        
     }
 }
 
