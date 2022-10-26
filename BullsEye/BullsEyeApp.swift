898bf060f182f436e7d27dd4c2956fd40e3ccb41
manukant tyagi <mankant.tyagi@ranium.in>
2022-10-26 12:14:49 +0530

    Initial Commit

---
+++ BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by manukant tyagi on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
